40484b0821a68390bd7455ede8bd03af25ab03ba
KhangHNG <gkhang2412@gmail.com>
2026-09-25 16:22:32 +0700

    Them Controller Api cho product

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductsApiController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
+
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/products', [ProductsApiController::class, 'index']);
+
 
 Route::middleware(['role:CUSTOMER,FARMER,ADMIN'])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
@@ -16,3 +22,4 @@ Route::middleware(['role:CUSTOMER'])->group(function () {
 Route::middleware(['role:FARMER'])->group(function () {
     Route::get('/farmer/ping', [AuthController::class, 'me']);
 });
+
