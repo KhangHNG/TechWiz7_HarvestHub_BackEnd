6ab96569ecae83bede14224f01c652912dbd4f24
Ha <gianghacd@gmail.com>
2026-09-26 15:41:25 +0700

    update config

--- config/mail.php
+++ config/mail.php
@@ -65,10 +65,6 @@ return [
             'transport' => 'resend',
         ],
 
-        'mailersend' => [
-            'transport' => 'mailersend',
-        ],
-
         'sendmail' => [
             'transport' => 'sendmail',
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
